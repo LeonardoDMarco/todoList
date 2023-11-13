42b23d8fe0077775e558ea46b86bd976ea5e57c6
LeonardoDMarco <leonardodemarco11@gmail.com>
2023-11-13 13:50:20 -0300

    readição de formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FormPerson from "./pages/FormPerson";
 import Evento from './ComponentEvent';
+import FormPersonRefactor from './pages/FormPerson/FormPessoaRefactor'
 
 function App() {
   return (
     <div className="App">
       <FormPerson/>,
       <Evento />
+      <FormPersonRefactor />
 
     </div>
   );
