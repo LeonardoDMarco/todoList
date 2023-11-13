2a985750ba8463271cf9dd98961746dc979dae9a
LeonardoDMarco <leonardodemarco11@gmail.com>
2023-11-13 18:04:56 -0300

    adicionei o condicional

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Evento from './ComponentEvent';
 import FormPersonRefactor from './pages/FormPerson/FormPessoaRefactor'
 import Condicional from './Condicional';
 
+
+
 function App() {
+
   return (
     <div className="App">
       <FormPerson/>,
       <Evento />
       <FormPersonRefactor />
       <Condicional />
-
     </div>
   );
 }
