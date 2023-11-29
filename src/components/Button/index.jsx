28bb27b05b029efa63591395e608952d8c532f92
Janio <janio.miara@somoseducacao.com.br>
2023-11-29 16:52:28 -0300

    ajustes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
+
 import React from "react";
 
-const Button = ({ nome }) => {
-  return <button>{nome}</button>;
-=======
-import React from 'react';
 import styled from 'styled-components';
+
+
+
 
 const ColorButton = styled.button`
 color: green;
@@ -13,20 +12,17 @@ background:red;
 font-size: 1em;
 margin: 1em;
 padding: 0.30em 1em;
-border radius: 5px;
-
 &:hover {background: #ff0fa8;
-color: black;}
+color: black;
 
 `;
-    
+
 
 const Button = ({nome}) => {
-    
+
     return (
         <ColorButton>Enviar!</ColorButton>
     );
->>>>>>> L04
 };
 
 export default Button;
